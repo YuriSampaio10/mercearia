c3f96453a06bebeb376679ea9cd5273b0d2d881a
Yuri Sampaio <sampaio.yuri31@gmail.com>
2026-03-22 17:32:32 -0300

    terminado a class DaoEstoque e fazendo a DaoFOrnecedor

--- DAO.py
+++ DAO.py
@@ -1,6 +1,6 @@
 from Models import *
 
-
+# DAO CATEGORIA
 class DaoCategoria:
     # salva no arquivo texto categoria txt
     @classmethod
@@ -24,7 +24,8 @@ class DaoCategoria:
             cat.append(Categoria(i))
 
         return cat  
-    
+
+# DAO VENDA
 class DaoVenda:
     # metodo salvar da DAO venda
     @classmethod
@@ -45,13 +46,76 @@ class DaoVenda:
         cls.venda = list(map(lambda x: x.replace('\n', ''), cls.venda))
         cls.venda = list(map(lambda x: x.split('|'), cls.venda))
        
-        vende = []
+        vend = []
         for i in cls.venda:
             produto = Produtos(i[0], i[1], i[2])
-            venda = Venda(produto, i[3], i[4], i[5])
-            vende.append(venda)
-        return vende
+            venda = Venda(produto, i[3], i[4], i[5], i[6])
+            vend.append(venda)
+        return vend
     
+# DAO ESTOQUE
+class DaoEstoque:
+    @classmethod
+    def salvar(cls, estoque):
+        with open('estoque.txt', 'a') as arq:
+            arq.writelines(estoque.produto.nome + '|' + estoque.produto.preco + '|' + estoque.produto.categoria + '|' + str(estoque.quantidade))
+            arq.writelines('\n')
 
-x = DaoVenda.ler()
-print(x)
+    
+    # abre o arquivo texto da estoque.txt
+    @classmethod
+    def ler(cls):
+        with open('estoque.txt', 'r') as arq:
+            # pega tudo que tem no arquivo texto e joga dentro de categoria
+            cls.estoque = arq.readlines()
+
+        # tira o \n
+        cls.estoque = list(map(lambda x: x.replace('\n', ''), cls.estoque))
+        cls.estoque = list(map(lambda x: x.split('|'), cls.estoque))
+
+        est = []
+        if len(cls.estoque) > 0:
+            for i in cls.estoque:
+                produto = Produtos(i[0], i[1], i[2])
+                estoque = Estoque(produto, i[3])
+                est.append(estoque)
+        print(cls.estoque)
+        return est
+    
+# adiciona o produto no estoque(nome, preco, categoria e quantidade) ao estoque e os lê
+# x = Produtos('macarrao', '8', 'alimento')
+# y = Estoque(x, '3')
+# DaoEstoque.salvar(y)
+# DaoEstoque.ler()
+
+
+# DAO FORNECEDOR
+class DaoFornecedor:
+    @classmethod
+    def salvar(cls, fornecedor: Fornecedor):
+        with open('fornecedores.txt', 'a') as arq:
+            arq.writelines(fornecedor.nome + '|' + fornecedor.cnpj + '|' + fornecedor.telefone + '|' + fornecedor.categoria)
+            arq.writelines('\n')
+
+    @classmethod
+    def ler(cls):
+        with open('fornecedores.txt', 'r') as arq:
+            # pega tudo que tem no arquivo texto e joga dentro de categoria
+            cls.fornecedores = arq.readlines()
+
+        # tira o \n
+        cls.fornecedores = list(map(lambda x: x.replace('\n', ''), cls.fornecedores))
+        cls.fornecedores = list(map(lambda x: x.split('|'), cls.fornecedores))
+
+        forn = []
+        for i in cls.fornecedores:
+            # produto = Produtos(i[0], i[1], i[2])
+            # venda = Venda(produto, i[3], i[4], i[5], i[6])
+            forn.append(Fornecedor(i[0], i[1], i[2], i[3]))
+
+        return forn
+    
+# adiciona o produto no estoque(nome, preco, categoria e quantidade) ao estoque e os lê
+x = Fornecedor('SA multimarcas', '858412928', '11958423655', 'alimenticios')
+DaoFornecedor.salvar(x)
+DaoFornecedor.ler()
