937eaa06253ddc422d569f29c721d3fb48009778
Yuri Sampaio <sampaio.yuri31@gmail.com>
2026-03-22 19:32:03 -0300

    atualizando o caminho txt

--- DAO.py
+++ DAO.py
@@ -5,14 +5,14 @@ class DaoCategoria:
     # salva no arquivo texto categoria txt
     @classmethod
     def salvar(cls, categoria):
-        with open('categoria.txt', 'a') as arq:
+        with open('txt/categoria.txt', 'a') as arq:
             arq.writelines(categoria)
             arq.writelines('\n')
 
     # abre o arquivo texto da categoria.txt
     @classmethod
     def ler(cls):
-        with open('categoria.txt', 'r') as arq:
+        with open('txt/categoria.txt', 'r') as arq:
             # pega tudo que tem no arquivo texto e joga dentro de categoria
             cls.categoria = arq.readlines()
             
@@ -30,7 +30,7 @@ class DaoVenda:
     # metodo salvar da DAO venda
     @classmethod
     def salvar(cls, venda: Venda):
-        with open('venda.txt', 'a') as arq:
+        with open('txt/venda.txt', 'a') as arq:
             arq.writelines(venda.itensVendido.nome + '|' + venda.itensVendido.preco + '|' + venda.itensVendido.categoria + '|' + venda.vendedor + '|' + venda.comprador + '|' + str(venda.quantidadeVendida) + '|' + venda.data)
             arq.writelines('\n')
 
@@ -38,7 +38,7 @@ class DaoVenda:
     # abre o arquivo texto da venda.txt
     @classmethod
     def ler(cls):
-        with open('venda.txt', 'r') as arq:
+        with open('txt/venda.txt', 'r') as arq:
             # pega tudo que tem no arquivo texto e joga dentro de categoria
             cls.venda = arq.readlines()
 
@@ -57,7 +57,7 @@ class DaoVenda:
 class DaoEstoque:
     @classmethod
     def salvar(cls, estoque):
-        with open('estoque.txt', 'a') as arq:
+        with open('txt/estoque.txt', 'a') as arq:
             arq.writelines(estoque.produto.nome + '|' + estoque.produto.preco + '|' + estoque.produto.categoria + '|' + str(estoque.quantidade))
             arq.writelines('\n')
 
@@ -65,7 +65,7 @@ class DaoEstoque:
     # abre o arquivo texto da estoque.txt
     @classmethod
     def ler(cls):
-        with open('estoque.txt', 'r') as arq:
+        with open('txt/estoque.txt', 'r') as arq:
             # pega tudo que tem no arquivo texto e joga dentro de categoria
             cls.estoque = arq.readlines()
 
@@ -93,13 +93,13 @@ class DaoEstoque:
 class DaoFornecedor:
     @classmethod
     def salvar(cls, fornecedor: Fornecedor):
-        with open('fornecedores.txt', 'a') as arq:
+        with open('txt/fornecedores.txt', 'a') as arq:
             arq.writelines(fornecedor.nome + '|' + fornecedor.cnpj + '|' + fornecedor.telefone + '|' + fornecedor.categoria)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open('fornecedores.txt', 'r') as arq:
+        with open('txt/fornecedores.txt', 'r') as arq:
             # pega tudo que tem no arquivo texto e joga dentro de categoria
             cls.fornecedores = arq.readlines()
 
@@ -126,13 +126,13 @@ class DaoFornecedor:
 class DaoPessoa:
     @classmethod
     def salvar(cls, pessoas: Pessoa):
-        with open('pessoa.txt', 'a') as arq:
+        with open('txt/pessoa.txt', 'a') as arq:
             arq.writelines(pessoas.nome + '|' + pessoas.telefone + '|' + pessoas.cpf + '|' + pessoas.email + '|' + pessoas.endereco)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open('pessoa.txt', 'r') as arq:
+        with open('txt/pessoa.txt', 'r') as arq:
             # pega tudo que tem no arquivo texto e joga dentro de categoria
             cls.clientes = arq.readlines()
 
@@ -159,13 +159,13 @@ class DaoPessoa:
 class DaoFuncionario:
     @classmethod
     def salvar(cls, funcionario: Funcionario):
-        with open('funcionario.txt', 'a') as arq:
+        with open('txt/funcionario.txt', 'a') as arq:
             arq.writelines(funcionario.nome + '|' + funcionario.telefone + '|' + funcionario.cpf + '|' + funcionario.email + '|' + funcionario.endereco + '|' + funcionario.clt)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open('funcionario.txt', 'r') as arq:
+        with open('txt/funcionario.txt', 'r') as arq:
             # pega tudo que tem no arquivo texto e joga dentro de categoria
             cls.funcionarios = arq.readlines()
 
@@ -183,6 +183,6 @@ class DaoFuncionario:
         return funcionarios
     
 # adiciona o produto no estoque(nome, preco, categoria e quantidade) ao estoque e os lê
-# x = Funcionario('yuri', '11958080950', '1519245253655', 'yuri@gmail.com', 'rua: 44hgffowi', '48182818')
+# x = Funcionario('lucas', '119580453950', '15889245253655', 'lucas@gmail.com', 'rua: 44hg8855owi', '481253318')
 # DaoFuncionario.salvar(x)
 # DaoFuncionario.ler()
